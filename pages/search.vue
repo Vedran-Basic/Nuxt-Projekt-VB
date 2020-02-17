2cd8d0ae1fc53e9fa5e4212bfac5d1d200e1a4e5
Vedran-Basic <vedran.basic98@gmail.com>
2020-02-17 08:41:19 +0100

    working on starting data fetching on search page

--- pages/search.vue
+++ pages/search.vue
@@ -1,16 +1,79 @@
 <template>
-  <div>
-    
+  <div class="container">
+    <div class="table-data">
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="5"
+        class="elevation-1"
+      ></v-data-table>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return{
+        url:'home',
+        homes:[],
+        headers:[],
+        localPagination:{
+          page:1,
+          searchText:''
+        },
+        pagination:{}
+      }
+    },
+    created(){
+      this.getData()
+    },
+    methods: {
+      async getData(){
+          if (!this.url) return
 
-  }
+          try {
+            const { page, searchText } = this.localPagination
+
+            const baseUrl = 'http://homehapp-api.jsteam.gaussx.com/api/'
+
+            const response = await this.$axios.get(baseUrl + this.url, {
+              params: {
+                page,
+                searchText,
+                sortBy,
+                sortOrder,
+                elastic: 1
+              }
+            })
+            const { data, pagination } = response.data.data
+            console.log({data, pagination})
+            if (data.length) {
+              const keys = Object.keys(data[0])
+              const headers = []
+              keys.forEach(k => {
+                console.log(typeof data[0][k])
+                if (typeof data[0][k] !== 'object') {
+                  headers.push({
+                    value: k,
+                    text: startCase(k)
+                  })
+                }
+              })
+              this.headers = headers
+            }
+
+            this.homes.splice(0, this.homes.length, ...data)
+            this.pagination = pagination
+          } catch (err) {
+            console.error(err)
+          }
+        }
+      }
+    }
 </script>
 <style scoped>
-  .header {
+  .container {
     display: flex;
     justify-content: center;
     flex-direction: column;
@@ -20,5 +83,8 @@
     background-position: top;
     background-size: cover;
     margin: 0 2% 0 2%;
+  }
+  .table-data{
+    display:block;
   }
 </style>
